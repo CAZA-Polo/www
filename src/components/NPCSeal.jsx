5b65005d6c3d5aed627f957af4695c35ec7c9047
John Lester Ymata <johnlester.ymata@cazatechnology.com>
2025-10-21 16:30:24 +0800

    for handover to JL

--- src/components/NPCSeal.jsx
+++ src/components/NPCSeal.jsx
@@ -1,7 +1,11 @@
 const NPCSeal = () => {
+
+    // Change image of NPC logo depending on the name of the logo uploaded to ./images/dpo folder
+    const npcLogo = 'npc-2025.png'
+
     return (
         <div className="flex justify-center items-center">
-            <img className="justify-center md:w-1/2 w-auto" alt="npc-seal" src="./images/dpo/npc-2025.png" />
+            <img className="justify-center md:w-1/2 w-auto" alt="npc-seal" src={`./images/dpo/${npcLogo}`} />
         </div>
     )
 }
